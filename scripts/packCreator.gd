8cd1ae4f2500f8b257159099bbc288c476ef2d35
Jacob Luebbert <jdlube@gmail.com>
2026-03-18 01:27:18 -0500

    Made cardDatabase global, started packCreator

---
+++ scripts/packCreator.gd
@@ -0,0 +1,11 @@
+extends Node
+
+
+func createPack():
+	var pack = []
+	
+	#to be coded... i'll probably need your help here Zach (i do have a basic idea)
+	#something like shuffle rarity arrays from cardDatabase, put x number of cards from each rarity array into new "pack" array, print pack
+	#was playing around a bit and found out i needed to make CardDatabase an autoloaded, global script/variable so that it could be used in other scripts, so that is done
+	#see: Project > Project Settings > Globals
+	#was also having a tough time figuring out how to test my code in the output tab, might need some help there
