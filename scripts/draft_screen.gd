c3f8deed87ecc14ee0fc12d9a25796436fe67faa
Zach Weineich <weinreych@gmail.com>
2026-03-31 20:50:33 -0500

    Added dynamic formatting to draft screen & added more test cards

--- scripts/draft_screen.gd
+++ scripts/draft_screen.gd
@@ -1,14 +1,71 @@
 extends Control
 
-@onready var buttonOne := $Button
-@onready var buttonTwo := $Button2
-@onready var buttonThree := $Button3
-@onready var buttonFour := $Button4
+# Scene definitions
+@onready var card := preload("res://scenes/cardButton.tscn")
+
+# Node definitions
+@onready var vbox = $CardSection/MarginContainer/VBoxContainer
+@onready var hbox = $CardSection/MarginContainer/VBoxContainer/HBoxContainer
+
+# Variable definitions
+var maxCardsPerRow: int = 4
+
+# @onready var buttonOneTextureRect := $CardSection/MarginContainer/VBoxContainer/HBoxContainer/Button5/TextureRect
+# @onready var buttonTwoTextureRect := $CardSection/MarginContainer/VBoxContainer/HBoxContainer/Button6/TextureRect
+# @onready var buttonThreeTextureRect := $CardSection/MarginContainer/VBoxContainer/HBoxContainer/Button7/TextureRect
+# @onready var buttonFourTextureRect := $CardSection/MarginContainer/VBoxContainer/HBoxContainer/Button8/TextureRect
+# @onready var buttonFiveTextureRect := $CardSection/MarginContainer/VBoxContainer/HBoxContainer/Button9/TextureRect
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	var pack = PackCreator.createPack()
-	var buttons = [buttonOne, buttonTwo, buttonThree, buttonFour]
+	# Generate pack
+	var containsMythic = randi_range(1,8)
+	var pack = PackCreator.createPack(10, 3, 0, 1) if containsMythic == 8 else PackCreator.createPack(10, 3, 1, 0)
+	#var buttons = [
+		#buttonOneTextureRect,
+		#buttonTwoTextureRect,
+		#buttonThreeTextureRect,
+		#buttonFourTextureRect,
+		#buttonFiveTextureRect
+	#]
+	
+	# Populate ui elements with cards
+	var current_hbox = hbox # Working hbox variable for this loop. Defaults to the original hbox
+	
+	print("number of cards: ", pack.size())
+	
 	for i in pack.size():
-		# buttons[i].text = pack[i]["rarity"] + "\n" + pack[i]["name"]
-		buttons[i].texture_normal = load(pack[i]["image"])
+		# Add a new hbox after every 4th card
+		if i > 0 and i % 4 == 0:
+			
+			# Clone original hbox & remove all child nodes
+			var new_hbox = hbox.duplicate()
+			for j in new_hbox.get_children():
+				j.queue_free()
+			
+			# Set working hbox variable to the new one
+			current_hbox = new_hbox
+			
+			# Add new hbox to scene as a child of the vbox
+			vbox.add_child(new_hbox) 
+			
+		# Create a new button node
+		var newButton = card.instantiate()
+		var newButtonTextureRect: TextureRect = newButton.get_node("TextureRect")
+		
+		# Set node properties
+		newButton.text = pack[i]["rarity"] + "\n" + pack[i]["name"]
+		newButtonTextureRect.texture = load(pack[i]["image"])
+		
+		# Add a button to the current working hbox
+		current_hbox.add_child(newButton)
+
+
+
+# WIP 3/30/26:
+#	- Convert texture buttons to buttons with texturerect child components.
+#	- Make rest of layout, add more cards to fill it out & add them in here.
+#	- BONUS: Get more card images to use for testing.
+
+#	- Setup Equipment Section
+#	- Think destiny-style loadout screen w/ card hovering.
--- scripts/packCreator.gd
+++ scripts/packCreator.gd
@@ -7,7 +7,7 @@ func _ready():
 #this needs to be tweaked obviously, but it works
 #also figured out why testing wasnt working, scripts must be attached to nodes to work with the "output" tab below, so i made a test node
 
-func createPack():
+func createPack(commons: int = 1, uncommons: int = 1, rares: int = 1, mythics: int = 1):
 	
 	#new variable "pack" is the final array before showing the player what they got
 	var pack = []
@@ -19,10 +19,14 @@ func createPack():
 	CardDatabase.mythics.shuffle()
 	
 	#takes the first selection from each rarity array puts it into the new "pack" array
-	pack += CardDatabase.commons.slice(0,1)
-	pack += CardDatabase.uncommons.slice(0,1)
-	pack += CardDatabase.rares.slice(0,1)
-	pack += CardDatabase.mythics.slice(0,1)
+	for i in commons:
+		pack += CardDatabase.commons.slice(i, i + 1)
+	for i in uncommons:
+		pack += CardDatabase.uncommons.slice(i, i + 1)
+	for i in rares:
+		pack += CardDatabase.rares.slice(i, i + 1)
+	for i in mythics:
+		pack += CardDatabase.mythics.slice(i, i + 1)
 	
 	#prints pack array, just for testing currently?
 	return pack
